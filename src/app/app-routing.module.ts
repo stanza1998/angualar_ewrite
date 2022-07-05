4a42f82d099f92100cf7ed1cb9848425cb565bec
STANZA JERRY NARIB <217144810@students.nust.na>
2022-07-05 16:37:02 +0200

    uploading mu project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { PriceComponent } from './price/price.component';
+import { ProductComponent } from './product/product.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{path:'login', component:LoginComponent},
+{path:'main', component: MainComponent},
+{path:'product', component: ProductComponent},
+{path:'price', component:PriceComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductComponent } from './product/product.component';
+import { PriceComponent } from './price/price.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProductComponent,
+    PriceComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
